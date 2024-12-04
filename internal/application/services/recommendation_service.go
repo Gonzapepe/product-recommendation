59fdb74dcb0b13eab765ace9f5b16f710deeb5df
Gonzapepe <addamogonzalo@gmail.com>
2024-12-04 02:02:22 -0300

    feat: added recommendation of similar products

--- internal/application/services/recommendation_service.go
+++ internal/application/services/recommendation_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"backend-challenge/internal/domain/entities"
 	"math"
+	"sort"
 	"strings"
 )
 
@@ -10,6 +11,68 @@ type RecommendationService struct {}
 
 func NewRecommendationService() *RecommendationService {
 	return &RecommendationService{}
+}
+
+// RecommendSimilarProducts recommends similar products based on a target product
+func (s *RecommendationService) RecommendSimilarProducts (targetProduct entities.Product, allProducts []*entities.Product) []*entities.Product {
+	targetVector := s.ExtractFeatureVector(targetProduct)
+
+	similarities := make([]struct {
+		Product *entities.Product
+		Similarity float64
+	}, len(allProducts))
+
+
+	// Calculate similarity for all products
+	for i, product := range allProducts {
+		if product.ID != targetProduct.ID { // Exclude the target product itself
+			productVector := s.ExtractFeatureVector(*product)
+			similarities[i] = struct{Product *entities.Product; Similarity float64}{
+				Product: product,
+				Similarity: s.CosineSimilarity(targetVector, productVector),
+			}
+		}
+	}
+
+	// Sort by similarity score
+	sort.Slice(similarities, func(i, j int) bool {
+		return similarities[i].Similarity > similarities[j].Similarity
+	})
+
+	// Return top 5 recommendations.
+	var recommendations []*entities.Product
+	for i := 0; i < len(similarities) && i < 5; i++ {
+		recommendations = append(recommendations, similarities[i].Product)
+	}
+
+	return recommendations
+}
+
+// CosineSimilarity computes the cosine similarity between two feature vectors
+func (s *RecommendationService) CosineSimilarity(vec1, vec2 map[string]float64) float64 {
+	// Extracted info on cosine similarity:
+	// https://en.wikipedia.org/wiki/Cosine_similarity
+
+	var dotProduct, magnitudeA, magnitudeB float64
+
+	for key, valueA := range vec1 {
+		valueB, ok := vec2[key]
+		if ok {
+			dotProduct += valueA * valueB
+		}
+
+		magnitudeA += valueA * valueA
+	}
+
+	for _, valueB := range vec2 {
+		magnitudeB += valueB * valueB
+	}
+
+	if magnitudeA == 0 || magnitudeB == 0 {
+		return 0 // Avoid division by zero
+	}
+
+	return dotProduct / (math.Sqrt(magnitudeA) * math.Sqrt(magnitudeB))
 }
 
 // ExtractFeatureVector creates a feature vector for a product
